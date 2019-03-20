845e0b35071d7ed54a230e7f02c824572df8092d
nglconsulting <nico.nglconsulting@gmail.com>
2019-03-20 17:03:04 +0100

    1st commit VR Architecture  Core projet UE4 VR
    Intoduction VR default Project UE4 C++

---
+++ Source/VR_Architecture/VR_ArchitectureGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VR_ArchitectureGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VR_ARCHITECTURE_API AVR_ArchitectureGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
